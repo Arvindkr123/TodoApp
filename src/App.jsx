061115a0ebb5e01d59ef394aeca8530518128911
Arvind <thakurarvindkr10@gmail.com>
2023-07-04 08:12:21 +0530

    added initial code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <input type="text" className='form-control'/>
+    </div>
+  )
+}
+
+export default App
