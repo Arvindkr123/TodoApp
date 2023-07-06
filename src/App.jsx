6a7a3f1c02d5f05de3ce541ef863e1495e2cb957
Arvind <thakurarvindkr10@gmail.com>
2023-07-06 08:14:05 +0530

    added the local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+import localforage from 'localforage';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [task, setTask] = useState({ name: '', completed: false });
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localforage.getItem('tasks', (err, storedTasks) => {
+      if (storedTasks) {
+        const parsedTasks = JSON.parse(storedTasks);
+        setTasks(parsedTasks);
+      }
+    });
+  }, []);
+
+  const storeTasks = (tasks) => {
+    setTasks(tasks);
+    const stringifiedTasks = JSON.stringify(tasks);
+    localforage.setItem('tasks', stringifiedTasks, (err) => {
+      if (err) {
+        console.error('Error storing tasks:', err);
+      } else {
+        console.log('Tasks stored successfully!');
+      }
+    });
+  };
+
   const getTasks = () => {
     return tasks.map((task, index) => (
       <li
-        class={
+        key={index}
+        className={
           task.completed
             ? 'list-group-item list-group-item-success'
             : 'list-group-item list-group-item-danger'
@@ -24,50 +48,53 @@ function App() {
       </li>
     ));
   };
+
   const updateTask = (i) => {
     const newTasks = [...tasks];
     newTasks.splice(i, 1, {
       name: newTasks[i].name,
       completed: !newTasks[i].completed,
     });
-    setTasks(newTasks);
+    storeTasks(newTasks);
   };
+
   const deleteTask = (i) => {
     const newTasks = [...tasks];
     newTasks.splice(i, 1);
-    setTasks(newTasks);
+    storeTasks(newTasks);
   };
+
   const addTask = (t) => {
     if (t) {
       const newTasks = [...tasks];
       newTasks.push({ name: t, completed: false });
-      setTasks(newTasks);
+      storeTasks(newTasks);
       setTask({ name: '', completed: false });
     } else {
-      alert('enter valid value');
+      alert('enter a valid value');
     }
   };
 
   return (
     <div className="App">
       <input
-        class="form-control"
+        className="form-control"
         type="text"
         onChange={(e) => {
-          setTask(e.target.value);
+          setTask({ ...task, name: e.target.value });
         }}
         value={task.name}
         placeholder="enter the task"
-      ></input>
+      />
       <button
-        class="btn btn-success w-100"
+        className="btn btn-success w-100"
         onClick={() => {
-          addTask(task);
+          addTask(task.name);
         }}
       >
         Change
       </button>
-      <ul class="list-group">{getTasks()}</ul>
+      <ul className="list-group">{getTasks()}</ul>
     </div>
   );
 }
